a56f9f687c70bddbe467cfb7fb762e8d1a5ce741
Abhijeet (AJ) Nage <abhijeet@ualberta.ca>
2024-02-01 21:44:00 -0800

    creating a table with components to understand react better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <FilterableProductTable product={PRODUCTS} />
   );
 }
 
-export default App;
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", prices: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", prices: "$1", stocked: true, name: "Peas"}
+]
+
+function FilterableProductTable({products}) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  )
+}
+
+function SearchBar(){
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
+  )
+}
+
+function ProductTable({products}) {
+  const rows = [];
+  let lastCategory = null;
+
+  products.forEach((product) => {
+    
+  })
+  
+  return (
+    <div>
+
+    </div>
+  )
+}
